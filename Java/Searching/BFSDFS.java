929b8ff939cf44621045b4311fd173b3ca659005
subham mishra <smsubhamengg@gmail.com>
2016-03-25 11:44:43 +0530

    Update BFSDFS.java
    BFS and DFS Implementation in java

--- Java/Searching/BFSDFS.java
+++ Java/Searching/BFSDFS.java
@@ -3,9 +3,9 @@ public void dfs() {
 		// DFS using Stack data structure
 		Stack stack = new Stack();
 		stack.push(this.rootNode);
-		rootNode.visited=true;
+		rootNode.visited=true; //mark pushed node as visited
 		printNode(rootNode);
-		while(!stack.isEmpty()) {
+		while(!stack.isEmpty()) { //confirm there is no underflow
 			Node node = (Node)s.peek();
 			Node child = getUnvisitedChildNode(n);
 			if(child != null) {
@@ -28,8 +28,8 @@ public void dfs() {
 		Queue queue = new LinkedList();
 		queue.add(this.rootNode);
 		printNode(this.rootNode);
-		rootNode.visited = true;
-		while(!queue.isEmpty()) {
+		rootNode.visited = true;  //mark pushed node as visited
+		while(!queue.isEmpty()) {  //confirm there is no underflow
 			Node node = (Node)queue.remove();
 			Node child=null;
 			while((child=getUnvisitedChildNode(node))!=null) {
