cdbf756c1c9c0bbca02ba5aadc4968f1c2c31c5e
subham mishra <smsubham342@gmail.com>
2016-04-18 00:39:28 +0530

    Update HeapSort.java
    Documentation improved

--- Java/Sorting/HeapSort.java
+++ Java/Sorting/HeapSort.java
@@ -1,26 +1,35 @@
-    /*
-
-     * Java Program to Implement Heap Sort
-
-     */
-
-     
-
     import java.util.Scanner;
 
+    /**
+     * Java Implementation of Heap Sort
+     * <code>
+     * procedure heapsort(a, count) is
+     *input: an unordered array a of length count
+     *(Build the heap in array a so that largest value is at the root)
+     *heapify(a, count)
+     *(The following loop maintains the invariants that a[0:end] is a heap and every element
+     *beyond end is greater than everything before it (so a[end:count] is in sorted order))
+     *end ← count - 1
+     *while end > 0 do
+     *  (a[0] is the root and largest value. The swap moves it in front of the sorted elements.)
+     *  swap(a[end], a[0])
+     *  (the heap size is reduced by one)
+     *  end ← end - 1
+     *  (the swap ruined the heap property, so restore it)
+     *  siftDown(a, 0, end)
+     * </code>
+     * 
+     * @author Subham Mishra
+     **/
      
-
-    /* Class HeapSort */
-
     public class HeapSort 
 
     {    
-
         private static int N;
 
         /* Sort Function */
 
-        public static void sort(int arr[])
+        public static void heapSort(int arr[])
 
         {       
 
@@ -43,109 +52,62 @@
         /* Function to build a heap */   
 
         public static void heapify(int arr[])
-
         {
-
             N = arr.length-1;
-
             for (int i = N/2; i >= 0; i--)
-
                 maxheap(arr, i);        
-
         }
 
-        /* Function to swap largest element in heap */        
+        /**
+        * 
+        *Function to swap largest element in heap
+        * 
+        **/        
 
         public static void maxheap(int arr[], int i)
-
         { 
-
             int left = 2*i ;
-
             int right = 2*i + 1;
-
             int max = i;
-
             if (left <= N && arr[left] > arr[i])
-
                 max = left;
-
             if (right <= N && arr[right] > arr[max])        
-
                 max = right;
-
-     
-
             if (max != i)
-
             {
-
                 swap(arr, i, max);
-
                 maxheap(arr, max);
-
             }
-
         }    
 
-        /* Function to swap two numbers in an array */
+        /**
+         * 
+        *Function to swap two numbers in an array 
+        *
+        **/
 
         public static void swap(int arr[], int i, int j)
-
         {
-
-            int tmp = arr[i];
-
+           int tmp = arr[i];
             arr[i] = arr[j];
-
             arr[j] = tmp; 
-
         }    
-
-        /* Main method */
-
+        
         public static void main(String[] args) 
-
         {
-
             Scanner scan = new Scanner( System.in );        
-
-            System.out.println("Heap Sort Test\n");
-
+            System.out.println("Heap Sort in java Demo");
             int n, i;    
-
-            /* Accept number of elements */
-
             System.out.println("Enter number of integer elements");
-
             n = scan.nextInt();    
-
-            /* Make array of n elements */
-
             int arr[] = new int[ n ];
-
-            /* Accept elements */
-
-            System.out.println("\nEnter "+ n +" integer elements");
-
+             System.out.println("\nEnter "+ n +" integer elements");
             for (i = 0; i < n; i++)
-
                 arr[i] = scan.nextInt();
-
-            /* Call method sort */
-
-            sort(arr);
-
-            /* Print sorted Array */
-
+            heapSort(arr);
             System.out.println("\nElements after sorting ");        
-
             for (i = 0; i < n; i++)
-
                 System.out.print(arr[i]+" ");            
-
             System.out.println();            
-
         }    
-
     }
